427195fe0adfe96dcd5b8dc684f82aca1f679156
Kyle Johnston <kylejohnston@Kyles-MacBook-Pro.local>
2020-08-19 22:16:47 -0400

    material + multiple posts w/ props

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from 'react';
 import logo from './grainery-logotype.png';
 import './App.css';
-import post from './post';
+import Post from './Post';
 
 function App() {
   return (
     <div className="App">
-      
-    
       <div className="app__header">
         <img
           className="app__headerImage"
           src={logo}
           alt="The Grainery"
         />
-        
-        
       </div>
     
-      <post />
-    
-    {/* Posts */}
-    {/* Posts */}
+      <Post username="Allison" caption="Another great shot" imageUrl="https://i.redd.it/sfw9ifrn5n751.jpg" />
+      <Post username="Kylespergallon" caption="second comment" imageUrl="https://i.redd.it/56bahl3rk9k41.jpg" />
+      <Post username="DonnyDrama" caption="thirrd comment" imageUrl="https://i.redd.it/56bahl3rk9k41.jpg" />
 
     </div>
   );
--- src/post.js
+++ src/post.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './Post.css';
+import Avatar from "@material-ui/core/Avatar";
 
-function post() {
+function Post({ username, caption, imageUrl }) {
     return (
-        <div>
-            <h3>Username</h3>
-            {/* header -> avatar + username */}
+        <div className="post">
+            <div className="post__header">
+                <Avatar
+                    className="post__avatar"
+                    alt={username}
+                    src="/static/images/avatar/1.jpg"
+                />
+                <h3>{username}</h3>
+                {/* header -> avatar + username */}
+            </div>
+            
 
+            <img className="post__image" src={imageUrl} alt="" />
             {/* image */}
 
+    <h4 className="post__text"><strong>{username}</strong> {caption}</h4>
             {/* username + caption */}
 
         </div>
     )
 }
 
-export default post
+export default Post
