bca022053d8d851038a709673721cc25b53b5ec5
Kyle Johnston <kylejohnston@Kyles-MacBook-Pro.local>
2020-08-20 23:38:31 -0400

    I have no idea where im commitiing these to

--- src/App.js
+++ src/App.js
@@ -2,10 +2,64 @@ import React, { useState, useEffect } from 'react';
 import logo from './grainery-logotype.png';
 import './App.css';
 import Post from './Post';
-import { db} from './firebase';
+import { db, auth } from './firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import { Button, Input } from '@material-ui/core';
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 
 function App() {
+  const classes = useStyles();
+  const [modalStyle] = useState(getModalStyle);
+
   const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+  const [user, setUser] = useState('null');
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // user has logged in...
+        console.log(authUser);
+        setUser(authUser);
+      } else {
+        // user has logged out...
+        setUser(null);
+      }
+    })
+
+    return () => {
+      // perform some cleanup actions
+      unsubscribe();
+    }
+
+  }, [user, username]);
 
     // useEffect -> runs a piece of code based on a specific condition
 
@@ -20,8 +74,64 @@ function App() {
       })
     }, []);
 
+    const signUp = (event) => {
+      event.preventDefault();
+
+      auth.createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        return authUser.user.updateProfile({ 
+          displayName: username,
+        })
+      })
+      .catch((error) => alert(error.message));
+    }
+
+
+
   return (
     <div className="App">
+      <Modal
+        open={open}
+        onClose={() => setOpen(false)}
+      >
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app__signup">
+            <center>
+              <img
+                className="app__headerImage"
+                src={logo}
+                alt="The Grainer"
+                />
+            </center>
+
+              <Input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+          
+              <Input
+                placeholder="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+
+              <Input 
+                placeholder="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              
+              <Button type="submit" onClick={signUp}>Sign Up</Button>
+          </form>
+          
+          
+        </div>
+      </Modal>
+
       <div className="app__header">
         <img
           className="app__headerImage"
@@ -30,6 +140,7 @@ function App() {
         />
       </div>
     
+    <Button onClick={() => setOpen(true)}>Sign Up</Button>
 
       {
         posts.map(({id, post}) => (
